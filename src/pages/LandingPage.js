b4431a50bc0ba859999aad42482ffda20e5955db
Yuda <yuda10aditya@gmail.com>
2021-03-01 18:45:43 +0700

    Finished create section component testimonial

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,6 +4,7 @@ import Hero from '../parts/Hero'
 import landingPage from '../json/landingPage.json';
 import MostPicked from '../parts/MostPicked'
 import Categories from '../parts/Categories'
+import Testimony from '../parts/Testimony'
 
 export default class LandingPage extends Component {
     constructor(props) {
@@ -17,6 +18,7 @@ export default class LandingPage extends Component {
               <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
               <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />  
               <Categories data={landingPage.categories} />
+              <Testimony data={landingPage.testimonial} />
             </>
         )
     }
